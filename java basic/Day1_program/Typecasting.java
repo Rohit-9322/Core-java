051804299ba9b408ae8aaec8ee08866ec6772e2c
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 19:24:43 +0530

    Create Typecasting.java

---
+++ java basic/Day1_program/Typecasting.java
@@ -0,0 +1,27 @@
+//package Day1_program;
+
+public class Typecasting {
+    public static void main(String[] args) {
+
+        // 1 Double to int 
+
+        double D = 10.21;
+        int A = (int)D;
+        System.out.println(A);
+
+        // 2 int to Double 
+        int C = 45;
+        double B = C;
+        System.out.println(B);
+
+        // 3 
+        int a = 150;
+        long b = a;
+        float c = b;
+
+        System.out.println(c);
+
+
+
+    }
+}
