fed72d9bf497ca4dd9a6d511eb4793cde6ad0d43
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 19:24:17 +0530

    Create wapper_class.java

---
+++ java basic/Day1_program/wapper_class.java
@@ -0,0 +1,12 @@
+//package Day1_program;
+
+public class wapper_class {
+    public static void main(String[] args) {
+        
+        int A = 10;
+        System.out.println("The Max-valu of Integer is :--> "+ Integer.MAX_VALUE);
+        System.out.println("Ihe Min value of Integer is :--> "+ Integer.MIN_VALUE);
+        System.out.println("Ihe Min value of Double is :--> " + Double.MAX_VALUE);
+        System.out.println("Ihe Min value of long is :--> "+Long.MAX_VALUE);
+    }
+}
