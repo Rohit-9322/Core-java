2180a163fe4bd97fe3de3f1d1998cfddd10d4f85
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 19:25:05 +0530

    Create T_overflow.java

---
+++ java basic/Day1_program/T_overflow.java
@@ -0,0 +1,9 @@
+//package Day1_program;
+
+public class T_overflow {
+    public static void main(String[] args) {
+        int Max = Integer.MAX_VALUE;
+        System.out.println("That Max :--> " + Max);
+        System.out.println("Ater the overflow "+ (Max+1));
+    }
+}
