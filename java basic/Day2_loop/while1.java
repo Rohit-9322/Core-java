0d101cc3e393bb6bf522e0043515dcd44a4e62ca
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 12:37:40 +0530

    Create while1.java

---
+++ java basic/Day2_loop/while1.java
@@ -0,0 +1,13 @@
+package Day2_loop;
+
+public class while1 {
+    
+    public static void main(String[] args) {
+        
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
