b992e6138ad8d651f65d1ef04077d08b20a9f136
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 19:25:37 +0530

    Create S_intrest.java

---
+++ java basic/Day1_program/S_intrest.java
@@ -0,0 +1,14 @@
+package Day1_program;
+
+public class S_intrest {
+
+    public static void main(String[] args) {
+        
+        double principal = 10000;
+        double rate  = 5.5;
+        double time = 3;
+
+        double SI = (principal + rate + time )/100;
+        System.out.println(SI);
+    }
+}
