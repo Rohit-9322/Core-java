69152abf0660cb1d9c11a194220a3dd48e1d4743
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 19:26:03 +0530

    Create PNZ.java

---
+++ java basic/Day1_program/PNZ.java
@@ -0,0 +1,21 @@
+package Day1_program;
+
+public class PNZ {
+    public static void main(String[] args) {
+        
+        // Q. The Number is Positive Nigitive or zero 
+
+        int n = 56;
+
+        if (n > 0){
+            System.out.println(" The n is Positive that is :--> " + n);
+        }
+        else if (n < 0) {
+            System.out.println("The n is Negative That is --> " +n);
+        }
+        else{
+            System.out.println(" The n is ZERO --> " + n);
+        }
+
+    }
+}
