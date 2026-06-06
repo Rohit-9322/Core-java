95674f71b75ffb00d860cd76e85d5650c9718fc0
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 19:26:27 +0530

    Create ASC2.java

---
+++ java basic/Day1_program/ASC2.java
@@ -0,0 +1,16 @@
+package Day1_program;
+
+public class ASC2 {
+    public static void main(String[] args) {
+        
+        // Printing A to z alphabite
+        char ch = 'A';
+        int acsii = ch;
+        System.out.println("ASC2 Is " + ch + " Is " + acsii); // 65
+
+        // a to z 
+        for(char c = 'A'; c <= 'Z'; c++){
+            System.out.print(c+" ");
+        }
+    }
+}
