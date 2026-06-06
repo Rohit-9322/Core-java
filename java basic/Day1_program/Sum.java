e0ded059e743dd0e2a7b444da1714c9a6ed315db
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-06 12:29:39 +0530

    Create Sum.java

---
+++ java basic/Day1_program/Sum.java
@@ -0,0 +1,13 @@
+//package Day1_program;
+
+public class Sum {
+    
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 12;
+
+        int C = A + B;
+        System.out.println("This is the Sum of A + B :--> " +C);
+    
+    }
+}
