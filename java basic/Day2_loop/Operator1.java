fabb9318bc561e28e4ba131997a79716fc525af1
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-07 11:25:50 +0530

    Create Operator1.java

---
+++ java basic/Day2_loop/Operator1.java
@@ -0,0 +1,16 @@
+package Day2_loop;
+
+public class Operator1 {
+    public static void main(String[] args) {
+            
+        // The number is divisible 3 and 5
+
+        int n = 30;
+        if ((n % 3 == 0) && (n % 5 == 0)) {
+            System.out.println("That divisible both ");
+        }
+        else{
+            System.out.println("Not divisible ");
+        }
+    }
+}
