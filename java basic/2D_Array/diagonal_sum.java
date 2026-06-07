4075caa3d391d25b4c06f915f586b0d99d1d4b68
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-07 20:01:45 +0530

    Create diagonal_sum.java

---
+++ java basic/2D_Array/diagonal_sum.java
@@ -0,0 +1,14 @@
+package 2D_Array;
+
+public class diagonal_sum {
+    public static void main(String[] args) {
+
+        // Diagonal sum 
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i][i];
+        }
+        System.out.println(sum);
+    }
+}
