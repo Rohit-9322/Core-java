fa66819fd10981cffb18865f4eb5b260a4cbb9d9
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-08 16:56:22 +0530

    Create add.java

---
+++ java basic/Method/add.java
@@ -0,0 +1,15 @@
+package Method;
+
+
+public class add {
+
+    static int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+       int n =  add(10, 20);
+       System.out.println(n);
+    }
+    
+}
