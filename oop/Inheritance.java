ab28136e93958cdeeba00471cd1f6a9d7ea4b029
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-08 19:22:26 +0530

    Create Inheritance.java

---
+++ oop/Inheritance.java
@@ -0,0 +1,54 @@
+//package oop;
+
+// Inheritance
+
+class base{
+    int x ;
+
+    void setx(int x){
+        this.x = x;
+    }
+
+    int getx(){
+        return x;
+    }
+
+    void display(){
+        System.out.println("I am Out");
+    }
+}
+
+
+class devede extends base{
+    int y;
+
+    void setx(int y){
+        this.y = y;
+    }
+
+    int getx(){
+        return y;
+    }
+
+    void display(){
+        System.out.println("I am Out");
+    }
+
+}
+public class Inheritance {
+    public static void main(String[] args) {
+
+        // base s1 = new base();
+        // s1.setx(12);
+        // s1.display();
+        // System.out.println(s1.getx());
+
+
+        devede s2 = new devede();
+        s2.setx(15);
+        System.out.println(s2.getx());
+        s2.display();
+        
+        
+    }
+}
