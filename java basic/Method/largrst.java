bc42aece2f82a56631c306579dbc4404e81abdd9
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-08 16:56:42 +0530

    Create largrst.java

---
+++ java basic/Method/largrst.java
@@ -0,0 +1,23 @@
+package Method;
+
+  // Largest of Two Numbers
+
+public class largrst {
+
+    static int max(int a , int b){
+    return (a>b)? a : b;
+    }
+    public static void main(String[] args) {
+        System.out.println(max(8, 3));
+    }
+
+
+    // Samallest 
+    static int min(int a , int b){
+    return (a<b)? a : b;
+    }
+    public static void main(String[] args) {
+        System.out.println(min(8, 3));
+    }
+    
+}
