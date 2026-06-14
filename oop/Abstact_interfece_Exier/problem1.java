2866a041b2a25d709cad169fc3d7f8e4b6daafdb
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-14 13:15:04 +0530

    Create problem1.java

---
+++ oop/Abstact_interfece_Exier/problem1.java
@@ -0,0 +1,10 @@
+package Abstact_interfece_Exier;
+
+// Crate an abstact class pen with method write() and refil() as abstract class 
+
+abstract class pen{
+  abstract  void write();
+    abstract void refil();
+}
+
+
