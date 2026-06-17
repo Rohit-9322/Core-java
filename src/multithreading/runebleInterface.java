d97f002b1be0fe4e3e2c2cbf4218881e5bdf6770
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-17 09:21:49 +0530

    Update runebleInterface.java

--- src/multithreading/runebleInterface.java
+++ src/multithreading/runebleInterface.java
@@ -1,15 +1,16 @@
 package All_class_file;
 
 // Implementing Runnble interface  
+// Using Thread(Runnable r)
 
 class A implements Runnable{
     @Override
    public void run(){
     int i = 0;
-    while (i < 10) {
-        System.out.println(" That is Thread 1");
-        i++;
-    }
+    // while (i < 10) {
+    //     System.out.println(" That is Thread 1");
+    //     i++;
+    // }
    }
 }
 
@@ -17,20 +18,28 @@ class B implements Runnable{
     @Override
    public void run(){
     int i = 0;
-    while (i < 10) {
-        System.out.println(" That is Thread 2");
-        i++;
-    }
+    // while (i < 10) {
+    //     System.out.println(" That is Thread 2");
+    //     i++;
+    // }
    }
 }
 public class runebleInterface {
     public static void main(String[] args) {
 
         A obj1 = new A();
-        Thread t1 = new Thread(obj1);
+
+        // Using Thread(Runnable r)
+        Thread t1 = new Thread(obj1,"ram");
+        System.out.println("The runnable id is "+ t1.getId());
+        System.out.println("Thread Nmae "+ t1.getName());
 
         B obj2 = new B();
-        Thread t2 = new Thread(obj2);
+
+        // Using Thread(Runnable r)
+        Thread t2 = new Thread(obj2,"sita");
+        System.out.println("The runnable id is "+ t2.getId());
+        System.out.println("Thread Nmae "+ t2.getName());
 
         t1.start();
         t2.start();
