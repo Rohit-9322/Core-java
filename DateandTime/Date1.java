22d4bab140c0e29ac6a8dd481612e2b30957c695
Wankhaderohit <rohitwankhade726@gmail.com>
2026-06-27 13:56:51 +0530

    Create Date1.java

---
+++ DateandTime/Date1.java
@@ -0,0 +1,15 @@
+package b_class;
+
+import java.util.Date;
+
+public class Date1 {
+    public static void main(String[] args) {
+       //  System.out.println(System.currentTimeMillis());     
+       
+       Date d = new Date();
+       System.out.println(d);
+
+       System.out.println(d.getTime());
+    }
+}
+// cd DateandTime
